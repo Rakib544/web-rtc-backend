011c8f64b3f60d831d1872c646b23f64c6dbccd5
Rakib544 <md.rakib10122003@gmail.com>
2021-10-25 00:51:34 +0600

    new commit

--- index.js
+++ index.js
@@ -5,32 +5,32 @@ const cors = require("cors");
 const io = require("socket.io")(server, {
   cors: {
     origin: "*",
-    method: ["GET", "POST"],
+    methods: ["GET", "POST"],
   },
 });
 
 app.use(cors());
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
-  res.send("Server is running...");
+  res.send("Running");
 });
 
 io.on("connection", (socket) => {
   socket.emit("me", socket.id);
 
   socket.on("disconnect", () => {
-    socket.broadcast.emit("callended");
+    socket.broadcast.emit("callEnded");
   });
 
-  socket.on("calluser", ({ userToCall, signalData, from, name }) => {
-    io.to(userToCall).emit("calluser", { signal: signalData, from, name });
+  socket.on("callUser", ({ userToCall, signalData, from, name }) => {
+    io.to(userToCall).emit("callUser", { signal: signalData, from, name });
   });
 
-  socket.on("answercall", (data) => {
-    io.to(data.to).emit("callaccepted", data.signal);
+  socket.on("answerCall", (data) => {
+    io.to(data.to).emit("callAccepted", data.signal);
   });
 });
 
-server.listen(PORT, () => console.log(`server listening on ${PORT}`));
+server.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
